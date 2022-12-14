03cf06ef6ffeeb85032708397ade61706a5b9f35
ElizabethHauroo <eliza-hauroo13@hotmail.com>
2022-12-14 11:01:05 +0000

    Register and Login Working

--- app/src/main/java/com/example/mynciapp/MainActivity.java
+++ app/src/main/java/com/example/mynciapp/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.mynciapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView createnewAccount;
+
+    EditText emailTF, pswdTF;
+    Button loginBTN;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        emailTF=findViewById(R.id.login_emailTF);
+        pswdTF=findViewById(R.id.login_PswdTF);
+        loginBTN=findViewById(R.id.login_btn);
+        progressDialog = new ProgressDialog(this);
+
+        mAuth=FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
 
         createnewAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,7 +55,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performLogin();
+            }
+        });
+
 
 
     } //override
+
+    private void performLogin() {
+        String email=emailTF.getText().toString();
+        String password=pswdTF.getText().toString();
+
+
+        if(!email.matches(emailPattern)){
+            emailTF.setError("Enter valid email");
+        }else if(password.isEmpty()){
+            pswdTF.setError("Please Enter a Password");
+        }else if(password.length()<6){
+            pswdTF.setError("Password must have at least 6 characters");
+        }else {
+            progressDialog.setMessage("Please wait while we log you in");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendHomeActivity();
+                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    }else{
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, ""+task.getException(),Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+
+
+        }// else
+
+
+    }
+
+    private void sendHomeActivity() {
+        Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/mynciapp/RegisterActivity.java
+++ app/src/main/java/com/example/mynciapp/RegisterActivity.java
@@ -1,17 +1,35 @@
 package com.example.mynciapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    // declaring variables
     TextView alreadyHaveAccount;
+    EditText emailTF, pswdTF, confirmPswdTF;
+    Button registerBTN;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
 
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +39,18 @@ public class RegisterActivity extends AppCompatActivity {
 
         alreadyHaveAccount=findViewById(R.id.reg_alreadyAcc);
 
+        // initializing all the variables
+        emailTF=findViewById(R.id.login_emailTF);
+        pswdTF=findViewById(R.id.reg_pswdTF);
+        confirmPswdTF=findViewById(R.id.login_PswdTF);
+        registerBTN=findViewById(R.id.reg_btn);
+        progressDialog = new ProgressDialog(this);
+
+        mAuth=FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+
+
 
 
         alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
@@ -28,8 +58,61 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(RegisterActivity.this, MainActivity.class));
             }
+        }); //already have an account?
+
+        registerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PerformAuthentication();
+            }
         });
 
 
     }// Override
+
+    private void PerformAuthentication() {
+        String email=emailTF.getText().toString();
+        String password=pswdTF.getText().toString();
+        String confirmPassward = confirmPswdTF.getText().toString();
+
+        if(!email.matches(emailPattern)){
+            emailTF.setError("Enter valid email");
+        }else if(password.isEmpty()){
+            pswdTF.setError("Please Enter a Password");
+        }else if(password.length()<6){
+            pswdTF.setError("Password must have at least 6 characters");
+        }else if(!password.equals(confirmPassward)){
+            confirmPswdTF.setError("Both passwords do not match");
+        }else{
+            progressDialog.setMessage("Please wait while we register your account");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendHomeActivity();
+                        Toast.makeText(RegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    }else{
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, ""+task.getException(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                
+            }); //authResult
+
+        }
+
+
+    }
+
+    private void sendHomeActivity() {
+        Intent intent=new Intent(RegisterActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
