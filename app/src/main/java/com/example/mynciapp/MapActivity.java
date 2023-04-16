4e1eb4ab874ebd4a14e4041795095a154d2cd347
ElizabethHauroo <eliza-hauroo13@hotmail.com>
2023-04-16 16:49:35 +0100

    search+spinner: WAD - replace with correct images

--- app/src/main/java/com/example/mynciapp/MapActivity.java
+++ app/src/main/java/com/example/mynciapp/MapActivity.java
@@ -7,12 +7,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -20,10 +25,29 @@ public class MapActivity extends AppCompatActivity {
     private ImageView imageBox;
     BottomNavigationView nav;
 
+    private AutoCompleteTextView autoCompleteTextView;
+    //private ImageView imageView;
+    private MaterialSpinner spinner;
+    private String[] map_room_names_for_search;
+    private int[] map_images;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        autoCompleteTextView = findViewById(R.id.search_autoCompleteTxt);
+        //imageView = findViewById(R.id.map_mainview);
+        spinner = findViewById(R.id.AllRooms_spinner);
+
+        map_room_names_for_search = getResources().getStringArray(R.array.map_room_names_for_search);
+        String[] mapImagesString = getResources().getStringArray((R.array.map_images));
+        map_images = new int[mapImagesString.length];
+        for(int i = 0; i < mapImagesString.length; i++ ){
+            map_images[i] = getResources().getIdentifier(mapImagesString[i], "drawable", getPackageName());
+        }
+        setupAutoCompleteTextView();
+        setupSpinner();
 
         first=(Button) findViewById(R.id.first_floor_map);
         second=(Button) findViewById(R.id.second_floor_map);
@@ -86,5 +110,52 @@ public class MapActivity extends AppCompatActivity {
             }
         });
 
-    }// onCreate
+    }// --------------------- onCreate------------------------------ onCreate------------------------
+
+    private void setupAutoCompleteTextView() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, map_room_names_for_search);
+        autoCompleteTextView.setAdapter(adapter);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                updateImageView(selectedItem);
+            }
+        });
+    }
+
+    private void updateImageView(String selectedItem) {
+        int index = -1;
+        for (int i = 0; i < map_room_names_for_search.length; i++) {
+            if (map_room_names_for_search[i].equals(selectedItem)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index >= 0) {
+            imageBox.setImageResource(map_images[index]);
+        }
+    }
+
+    private void setupSpinner() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, map_room_names_for_search);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
+                updateImageView(map_room_names_for_search[position]);
+            }
+        });
+        spinner.setOnNothingSelectedListener(new MaterialSpinner.OnNothingSelectedListener() {
+            @Override
+            public void onNothingSelected(MaterialSpinner spinner) {
+                // Don't do anything
+            }
+        });
+    }
+
+
+
 }
