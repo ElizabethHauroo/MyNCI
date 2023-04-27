b0d36ef6682c467e1c96dbb382625207db742b67
ElizabethHauroo <eliza-hauroo13@hotmail.com>
2023-04-27 23:44:46 +0100

    logo animation:WAD

--- app/src/main/java/com/example/mynciapp/MainActivity.java
+++ app/src/main/java/com/example/mynciapp/MainActivity.java
@@ -149,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendHomeActivity() {
-        Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+        Intent intent=new Intent(MainActivity.this,AnimationActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
